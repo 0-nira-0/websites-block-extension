e29c0a3f6b7a60d940e0d8183f960b5aa30755ae
Mykyta Oliinyk <mykyta.oliinyk0@gmail.com>
2026-05-01 21:19:04 +0200

    feat: design tokens

--- src/complete/main.tsx
+++ src/complete/main.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import "../styles/reset.css";
+import "../styles/tokens.css";
 import Complete from "./Complete";
 
 createRoot(document.getElementById("root")!).render(
